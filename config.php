9da137758809e0401d900fbf7f4893e47e04a83b
kavindha <kavindharanasinghe@gmail.com>
2021-05-18 23:31:52 +0530

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    define( "SCF_DB_HOST", "147.135.37.75" );
-    define( "SCF_DB_USER", "kavindha@abtest" );
-    define( "SCF_DB_PASSWORD", "@K@vindh@2020" );
-    define( "SCF_DB_NAME", "abtesting_dev" );
